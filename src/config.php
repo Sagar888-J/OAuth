d467bcfcf4a113be7fdb09471c14fd799973b208
[Sagar J] <[sagarj5000@gmail.com]>
2025-02-17 15:21:53 +0530

    Config file

--- src/config.php
+++ src/config.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    "client_id"     => "938591792831-cah2komn3hbp8ddturhhvkaf003qbuhi.apps.googleusercontent.com",
-    "client_secret" => "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",
-    "redirect_uri"  => "http://localhost/oauth-module/code.php",
-    "token_url"     => "https://oauth2.googleapis.com/token",
-    "auth_url"      => "https://accounts.google.com/o/oauth2/auth",
-    "scopes"        => "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/spreadsheets.readonly https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly",
+    "client_id"     => "",
+    "client_secret" => "",
+    "redirect_uri"  => "",
+    "token_url"     => "",
+    "auth_url"      => "",
+    "scopes"        => "",
 ];
