cb6c39dfc4055bc890c2672d34e4f63ddfcb3634
[Sagar J] <[sagarj5000@gmail.com]>
2025-02-17 14:21:14 +0530

    OAuth 2.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sagarj/oauth-module',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7b01e50776c763da335e26856dc993c0ba423154',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sagarj/oauth-module' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7b01e50776c763da335e26856dc993c0ba423154',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
